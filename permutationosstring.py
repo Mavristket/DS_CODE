4c465f02bf4f8d88742090c083be8bc6d08bc70b
Balaji Gaikwad <gaikwadb602@gmail.com>
2026-03-18 15:48:15 +0530

    Add files via upload

---
+++ permutationosstring.py
@@ -0,0 +1,12 @@
+def permute(s, ans=""):
+    if len(s) == 0:
+        print(ans)
+        return
+
+    for i in range(len(s)):
+        ch = s[i]
+        rest = s[:i] + s[i+1:]
+        permute(rest, ans + ch)
+
+# Example
+permute("ABC")
